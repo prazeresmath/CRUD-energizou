37f5bf4e8068f82d3e5a4bf3a07200c437805acc
prazeresmath <matheus.prazeres@hotmail.com>
2023-10-30 12:59:07 -0300

    tela do frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,42 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Clientes from './Clientes';
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Container = styled.div`
+
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display> flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+
+`;
+
+const Title = styled.h2`
+
+
+
+`;
+
 
 function App() {
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Clientes />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+    <Container>
+      <Title>USUÁRIOS</Title>
+      <Form />
+    </Container>
+    <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    
+    </>
   );
-}
+};
+
 
 export default App;
